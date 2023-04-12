7d46588ad2ae4a112ae7194b22fcaaaf878e88b0
16amattice <anthonymattice@gmail.com>
2023-04-12 11:50:26 -0500

    Add project files.

---
+++ Controllers/Blackjack.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class Blackjack : Controller
+{
+    public IActionResult Index()
+    {
+        return View("Index");
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace drew_blackjack_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
